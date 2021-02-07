0186af4d4d9e21fb5e6c6ef14cfd04da9f798cd9
JuanfeRomero <juanferomerocode@gmail.com>
2021-02-07 12:15:58 -0300

    exercises of courseinfo complete...for now

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje';
+
+const Description = (props) => {
+  return <p>Hola, {props.name} tiene {props.age} años</p>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Mensaje color='red' name='FEDE'/>
+            <Description name="Juanfe" age="25"/>
+            <Mensaje color='green' name='TITI'/>
+            <Description name="Virginia" age="33"/>
+            <Mensaje color='yellow' name='GUADA'/>
+            <Description name="Guadalupe" age="14"/>
+        </div>
+    );
 }
 
 export default App;
